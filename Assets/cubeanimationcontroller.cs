5ac583584583d424267a5d827cb0ac6667b5013f
dgutsav <dgutsav@gmail.com>
2020-04-01 12:31:55 +0530

    first commit

---
+++ Assets/cubeanimationcontroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeanimationcontroller : MonoBehaviour
+{
+    private bool isAnimating = false;
+    private Animator animator;
+    void Start(){
+        animator = GetComponent<Animator>();
+    }
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Debug.Log("Space Pressed"); 
+            if(isAnimating){
+                animator.SetBool("inAnimation", false);
+                isAnimating = false;
+            }
+            else{
+                isAnimating = true;
+                animator.SetBool("inAnimation",true);
+            }
+        }
+    }
+}
